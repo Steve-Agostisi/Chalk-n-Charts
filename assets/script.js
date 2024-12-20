0acae40c5a1a3f42dcf5c7651874a6953c9caa30
CodingNija <hunterkile56@gmail.com>
2024-12-19 20:56:42 -0500

    added code for the modal

--- assets/script.js
+++ assets/script.js
@@ -5,33 +5,22 @@
 
 
 //modal code
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+window.onload = function () {
+    const modal = document.getElementById("myModal");
+    const closeModalButton = document.getElementById("close-modal");
+  
+    // Display the modal 
+    modal.style.display = "block";
+  
+    // Close the modal when the close button is clicked
+    closeModalButton.addEventListener("click", function () {
+      modal.style.display = "none";
+    });
+  
+    // Close the modal when clicking outside of it
+    modal.addEventListener("click", function () {
+      modal.style.display = "none";
+    });
+  };
 
 //profile button
