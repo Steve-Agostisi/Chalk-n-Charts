3328707dc7c607b9d499fd85822a6e19a62bf99c
Steven Agostisi <sagostisi@gmail.com>
2024-12-19 21:01:13 -0500

    profile-html

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // variables
 document.getElementById("myButton").onclick = function () {
-  location.href = "www.yoursite.com";
+  location.href = "http://127.0.0.1:5500/Chalk-n-Charts/Chalk-n-Charts/assets/profile.html";
 };
 
 
